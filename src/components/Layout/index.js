b907ebbd593a704332a300cafe42b62cbd53f10f
Michal Pawluczuk <mi23chal@gmail.com>
2020-07-24 17:59:34 +0200

    feat: add layoutValues logic to Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
@@ -7,13 +7,59 @@ import { theme, GlobalStyle } from 'assets/styles';
 import Navbar from 'views/Navbar';
 import { Main } from './styles';
 
-const Layout = ({ children }) => (
-	<ThemeProvider theme={theme}>
-		<GlobalStyle />
-		<Navbar id="navbar" />
-		<Main>{children}</Main>
-	</ThemeProvider>
-);
+export const marginContext = createContext({
+	dynamicMargin: 0,
+	actualWidth: 0,
+});
+
+const contentWidth = parseInt(theme.value.contentWidth, 10);
+const maxWidth = parseInt(theme.value.maxWidth, 10);
+
+const Layout = ({ children }) => {
+	const { Provider: MarginProvider } = marginContext;
+
+	const [layoutValues, setLayoutValues] = useState({
+		dynamicMargin: 0,
+		actualWidth: 0,
+	});
+
+	useEffect(() => {
+		const getCardWidth = () => {
+			const documentWidth = document
+				.querySelector('body')
+				.getBoundingClientRect().width;
+			const newWidth = documentWidth < maxWidth ? documentWidth : maxWidth;
+
+			if (newWidth <= contentWidth) {
+				setLayoutValues({
+					dynamicMargin: 0,
+					actualWidth: newWidth,
+				});
+			}
+
+			if (newWidth > contentWidth) {
+				setLayoutValues({
+					dynamicMargin: (newWidth - contentWidth) / 2,
+					actualWidth: newWidth,
+				});
+			}
+		};
+
+		getCardWidth();
+		window.addEventListener('resize', getCardWidth);
+		return () => window.removeEventListener('resize', getCardWidth);
+	}, []);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<MarginProvider value={layoutValues}>
+				<Navbar id="navbar" />
+				<Main>{children}</Main>
+			</MarginProvider>
+		</ThemeProvider>
+	);
+};
 
 Layout.propTypes = {
 	children: PropTypes.node.isRequired,
